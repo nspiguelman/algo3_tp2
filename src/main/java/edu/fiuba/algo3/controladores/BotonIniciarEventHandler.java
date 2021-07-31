8ff98288ffcbaf72dcc5de9b773ef01cf5527c71
franciscoinsua-fiuba <finsua@fi.uba.ar>
2021-07-31 17:40:31 -0300

    Push branch

--- src/main/java/edu/fiuba/algo3/controladores/BotonIniciarEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controladores/BotonIniciarEventHandler.java
@@ -3,18 +3,23 @@ package edu.fiuba.algo3.controladores;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 
 public class BotonIniciarEventHandler implements EventHandler<ActionEvent> {
     private Button unBoton;
 
-    public BotonIniciarEventHandler(Button unBoton) {
-        this.unBoton = unBoton;
+
+    public BotonIniciarEventHandler(Boolean jugadorAzul, Boolean jugadorRojo, Boolean jugadorVerde, Boolean jugadorNaranja, Boolean jugadorNegro, Boolean jugadorRosa) {
+
     }
 
+    public void inicializar(){
+
+    }
 
 
     @Override
     public void handle(ActionEvent actionEvent){
-       System.out.println("Hola");
+        System.out.println("Hola");
     }
 }
--- src/main/java/edu/fiuba/algo3/vistas/ContenedorEntrada.java
+++ src/main/java/edu/fiuba/algo3/vistas/ContenedorEntrada.java
@@ -1,67 +1,82 @@
 package edu.fiuba.algo3.vistas;
 
+import edu.fiuba.algo3.controladores.BotonIniciarEventHandler;
 import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.geometry.Insets;
 
 public class ContenedorEntrada {
     private VBox contenedorEntrada;
+    private int botonCordX = 200;
+    private int botonCordY = 400;
 
     public ContenedorEntrada() {
         StackPane layout = new StackPane();
         layout.setStyle("-fx-background-color: #FFECB9");
 
+        Image imagen = new Image("file:src/main/java/edu/fiuba/algo3/imagenes/teg.jpeg");
+        BackgroundPosition backgroundImagePosition = new BackgroundPosition(null, 300,false,null, 20, false);
 
-        //Image imagen = new Image("teg.jpeg");
+        BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, backgroundImagePosition, BackgroundSize.DEFAULT);
         //final ImageView imagenVista = new ImageView(imagen);
 
         Label etiqueta = new Label();
 
-        TextField texto = new TextField();
-        texto.setPromptText("Jugador Uno");
-        texto.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #0077bb; -fx-font-weight: 900; -fx-max-width: 200px");
+        CheckBox jugadorAzul = new CheckBox("Azul");
+        jugadorAzul.setIndeterminate(false);
+        jugadorAzul.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #0077bb; -fx-font-weight: 900; -fx-max-width: 200px");
 
-        TextField texto2 = new TextField();
-        texto2.setPromptText("Jugador Dos");
-        texto2.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #cc3311;-fx-font-weight: 900; -fx-max-width: 200px");
+        CheckBox jugadorRojo = new CheckBox("Rojo");
+        jugadorRojo.setIndeterminate(false);
+        jugadorRojo.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #cc3311;-fx-font-weight: 900; -fx-max-width: 200px");
 
-        TextField texto3 = new TextField();
-        texto3.setPromptText("Jugador Tres");
-        texto3.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #ee7733;-fx-font-weight: 900; -fx-max-width: 200px");
+        CheckBox jugadorVerde = new CheckBox("Verde");
+        jugadorVerde.setIndeterminate(false);
+        jugadorVerde.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white  ; -fx-background-color: #009988;-fx-font-weight: 900; -fx-max-width: 200px");
 
+        CheckBox jugadorNaranja = new CheckBox("Naranja");
+        jugadorNaranja.setIndeterminate(false);
+        jugadorNaranja.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #ee7733;-fx-font-weight: 900; -fx-max-width: 200px");
 
-        TextField texto4 = new TextField();
-        texto4.setPromptText("Jugador Cuatro");
-        texto4.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white  ; -fx-background-color: #009988;-fx-font-weight: 900; -fx-max-width: 200px");
+        CheckBox jugadorNegro = new CheckBox("Negro");
+        jugadorNegro.setIndeterminate(false);
+        jugadorNegro.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #000000;-fx-font-weight: 900 ; -fx-max-width: 200px");
 
-        TextField texto5 = new TextField();
-        texto5.setPromptText("Jugador Cinco");
-        texto5.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white  ; -fx-background-color: #ee3377;-fx-font-weight: 900; -fx-max-width: 200px");
+        CheckBox jugadorRosa = new CheckBox("Rosa");
+        jugadorRosa.setIndeterminate(false);
+        jugadorRosa.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white  ; -fx-background-color: #ee3377;-fx-font-weight: 900; -fx-max-width: 200px");
 
-        TextField texto6 = new TextField();
-        texto6.setPromptText("Jugador Seis");
-        texto6.setStyle("-fx-text-box-border: black; -fx-font-scale: 20 ; -fx-text-fill: white ; -fx-background-color: #000000;-fx-font-weight: 900 ; -fx-max-width: 200px");
 
         Button botonIniciar = new Button();
 
         botonIniciar.setText("Iniciar Juego");
         botonIniciar.setAlignment(Pos.BOTTOM_CENTER);
 
-        botonIniciar.setTranslateX(200);
-        botonIniciar.setTranslateX(400);
-
+        botonIniciar.setTranslateX(botonCordX);
+        botonIniciar.setTranslateX(botonCordY);
+        BotonIniciarEventHandler handler = new BotonIniciarEventHandler(
+                jugadorAzul.isSelected(),
+                jugadorRojo.isSelected(),
+                jugadorVerde.isSelected(),
+                jugadorNaranja.isSelected(),
+                jugadorNegro.isSelected(),
+                jugadorRosa.isSelected()
+        );
+        //botonIniciar.setOnAction(handler);
         HBox contenedorHorizontal = new HBox(botonIniciar);
 
-        VBox contenedorPrincipal = new VBox(texto, texto2, texto3, texto4, texto5, texto6, contenedorHorizontal, etiqueta);
+        VBox contenedorPrincipal = new VBox(jugadorAzul, jugadorRojo, jugadorVerde, jugadorNaranja, jugadorNegro, jugadorRosa, contenedorHorizontal, etiqueta);
 
         contenedorPrincipal.setAlignment(Pos.CENTER);
         contenedorPrincipal.setSpacing(20);
         contenedorPrincipal.setPadding(new Insets(25));
+        contenedorPrincipal.setBackground(new Background(imagenDeFondo));
 
         this.contenedorEntrada = contenedorPrincipal;
     }
